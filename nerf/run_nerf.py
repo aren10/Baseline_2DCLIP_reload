b93aca340f8bfe253aa8634590f5318415a0cbed
Jiahao Ren <86091361+aren10@users.noreply.github.com>
2022-10-19 01:49:35 -0400

    new

--- nerf/run_nerf.py
+++ nerf/run_nerf.py
@@ -994,7 +994,7 @@ def train(env, flag, test_file, i_weights):
         rays_rgb = torch.Tensor(rays_rgb).to(device)
 
     #______________________________________
-    N_iters = 10000 + 1
+    N_iters = 10000
 
     losses = []
     # Summary writers
